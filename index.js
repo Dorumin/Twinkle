8b9292c59c38feafcc105ab86ad5b003307af2ba
Dorumin <dorminsama@gmail.com>
2019-10-09 19:19:01 -0300

    fix source mappings

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const path = require('path');
 const Doruphie = require('./src/doruphie.js');
 const client = new Doruphie();
 
-client.loadPluginDir(path.join(__dirname, 'plugins'));
+client.loadPluginDir(path.join(__dirname, 'src', 'plugins'));
 
-client.commander.loadCommandDir(path.join(__dirname, 'plugins', 'commander', 'commands'));
+client.commander.loadCommandDir(path.join(__dirname, 'src', 'plugins', 'commander', 'commands'));
 
 client.login(client.config.TOKEN);
