927946703ea338f7348c4d2bf4567e0c47ffc025
Dorumin <dorminsama@gmail.com>
2019-10-09 23:19:10 -0300

    Flesh out mass mention filter

--- src/plugins/automod/filters/MassMention.js
+++ src/plugins/automod/filters/MassMention.js
@@ -1,8 +1,18 @@
 const Filter = require('../structs/filter.js');
 
 class MassMentionFilter extends Filter {
+    constructor(automod) {
+        super(automod);
+
+        this.USERS_PATTERN = /<@!?\d+>/g;
+    }
+
+    suppressMentions(text) {
+        return text.replace(this.USERS_PATTERN, '');
+    }
+
     interested(message) {
-        if (message.member.permissions.hasPermission('MANAGE_MESSAGES')) return false;
+        // if (message.member.permissions.has('MANAGE_MESSAGES')) return false;
 
         if (message.mentions.users.size < 6) return;
 
@@ -10,7 +20,22 @@ class MassMentionFilter extends Filter {
     }
 
     handle(message) {
-        console.log('Too many mentions:', message.content);
+        const suppressed = this.suppressMentions(message.content);
+
+        if (!suppressed.trim()) {
+            message.delete();
+        }
+
+        message.channel.send({
+            embed: {
+                title: `${message.author.username}#${message.author.discriminator} has been warned`,
+                color: message.guild.me.displayColor,
+                description: `<@${message.author.id}>\nReason: Mass mention (${message.mentions.users.size})`, // TODO: # of offenses
+                author: {
+                    icon_url: message.author.displayAvatarURL
+                }
+            }
+        });
     }
 }
 
