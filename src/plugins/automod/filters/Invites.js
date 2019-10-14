7d56ae09b883b873bfd6b45b83d7088159698d52
Dorumin <dorminsama@gmail.com>
2019-10-14 00:21:26 -0300

    typo

--- src/plugins/automod/filters/Invites.js
+++ src/plugins/automod/filters/Invites.js
@@ -15,7 +15,7 @@ class InvitesFilter extends Filter {
         if (message.member.permissions.has('MANAGE_MESSAGES')) return false;
 
         const inviteCodes = this.matchInvites(message.content);
-        if (!inviteCode) return false;
+        if (!inviteCodes) return false;
 
         let invites;
         try {
