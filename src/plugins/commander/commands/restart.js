7d44e158d4b21a0a662b251e7bec608f587cd800
Dorumin <dorminsama@gmail.com>
2019-10-14 01:18:34 -0300

    Make !restart support heroku env

--- src/plugins/commander/commands/restart.js
+++ src/plugins/commander/commands/restart.js
@@ -6,16 +6,46 @@ class RestartCommand extends OPCommand {
         super(bot);
         this.aliases = ['restart', 'r'];
         this.hidden = true;
+        this.heroku = this.bot._globalConfig.HEROKU == true;
+        console.log(this.bot._globalConfig.HEROKU, typeof this.bot._globalConfig.HEROKU)
     }
 
-    async call(message, content) {
+    async call(message) {
+        await message.channel.send('Restarting...');
+
+        if (this.heroku) {
+            this.restartHeroku();
+        } else {
+            this.restartProc();
+        }
+    }
+
+    async restartHeroku() {
+        const config = this.bot._globalConfig;
+        const name = config.IS_BACKUP ? config.APP_NAME : config.BACKUP_APP_NAME;
+        const token = config.HEROKU_TOKEN;
+
+        const { body } = await got(`https://api.heroku.com/teams/apps/${name}`, {
+            json: true,
+            headers: {
+                Accept: `application/vnd.heroku+json; version=3`,
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        await got.delete(`https://api.heroku.com/apps/${body.id}/dynos`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    }
+
+    restartProc() {
         const subprocess = spawn(process.argv0, process.argv.slice(1), {
             detached: true,
             stdio: 'ignore'
         });
         subprocess.unref();
-
-        await message.channel.send('Restarting...');
 
         process.exit(0);
     }
