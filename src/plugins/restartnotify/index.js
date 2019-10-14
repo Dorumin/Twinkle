fc3190c32747f5c559e880d78c9a24b92244eb9b
Dorumin <dorminsama@gmail.com>
2019-10-14 14:07:50 -0300

    Restart success notify in the same channel

---
+++ src/plugins/restartnotify/index.js
@@ -0,0 +1,35 @@
+const Plugin = require('../../structs/plugin.js');
+const DatabasePlugin = require('../db');
+
+class RestartNotifyPlugin extends Plugin {
+    load() {
+        this.bot.restartNotify = new RestartNotify(this.bot);
+    }
+}
+
+class RestartNotify {
+    constructor(bot) {
+        this.bot = bot;
+        bot.client.on('ready', this.onReady.bind(this));
+    }
+
+    static get deps() {
+        return [
+            DatabasePlugin
+        ];
+    }
+
+    async onReady() {
+        const channelId = this.bot.db.get('lastRestartChannel');
+        if (!channelId) return;
+
+        this.bot.db.delete('lastRestartChannel');
+
+        const channel = this.bot.client.channels.get(channelId);
+        if (!channel) return;
+
+        channel.send('Restarted!');
+    }
+}
+
+module.exports = RestartNotifyPlugin;
