599db5652fbfca6b74bc6a0ef49c4e17f44fabdc
Dorumin <dorminsama@gmail.com>
2019-10-13 22:51:46 -0300

    Add fandomizer plugin for url resolving

---
+++ src/plugins/fandomizer/index.js
@@ -0,0 +1,39 @@
+const got = require('got');
+const Plugin = require('../../structs/plugin.js');
+
+class FandomizerPlugin extends Plugin {
+    load() {
+        this.bot.fandomizer = new Fandomizer(this.bot);
+    }
+}
+
+class Fandomizer {
+    constructor(bot) {
+        this.cache = new Map();
+    }
+
+    fandomize(wikiname) {
+        if (this.cache.has(wikiname)) {
+            this.cache.set(wikiname, this.fetch(wikiname));
+        }
+
+        return this.cache.get(wikiname);
+
+    }
+
+    async fetch(wikiname) {
+        // Note: http necessary
+        const { headers } = await got.head(`http://${wikiname}.wikia.com/api.php`, { followRedirects: false }),
+        url = new URL(headers.location),
+        parts = url.pathname.split('/'),
+        host = url.hostname;
+
+        parts.pop();
+
+        const url = `https://${host}${parts.join('/')}`;
+
+        return url;
+    }
+}
+
+module.exports = FandomizerPlugin;
