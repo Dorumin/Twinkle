8863a1644bf7f607f7751f686895fbe3e3476e4d
Dorumin <dorminsama@gmail.com>
2019-10-18 18:40:56 -0300

    Add common cache structure

---
+++ src/structs/cache.js
@@ -0,0 +1,25 @@
+const Collection = require('./Collection');
+
+class Cache extends Collection {
+    get(key, generator) {
+        if (!this.has(key)) {
+            const val = generator();
+            this.set(key, val);
+            return val;
+        }
+
+        return this.get(key);
+    }
+
+    getAll(generators) {
+        const rets = [];
+
+        for (const key in generators) {
+            rets.push(this.get(key, generators[key]));
+        }
+
+        return rets;
+    }
+}
+
+module.exports = Cache;
