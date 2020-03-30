adff392187cb00f08728b2881f55b7610c53cc16
Naughty <dorminsama@gmail.com>
2020-03-30 11:28:54 -0300

    ok but do the bot filter right this time

--- src/plugins/logger/index.js
+++ src/plugins/logger/index.js
@@ -29,7 +29,7 @@ class Logger {
     }
 
     onMessage(message) {
-		if (message.author.id === this.bot.client.user.id) return;
+		if (message.author.bot && message.author.id !== this.bot.client.user.id) return;
 		// if (this.config.CHANNEL && message.channel.id == this.config.CHANNEL) return;
 
         const place = message.channel.type == 'dm'
