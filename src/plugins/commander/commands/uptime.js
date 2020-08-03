e8ea1a09c5fc783b8121cb6ae170ab29515e29fd
Robyn <32017811+GameModerator@users.noreply.github.com>
2020-08-03 18:06:56 +0800

    one-lining shit and changing to method

--- src/plugins/commander/commands/uptime.js
+++ src/plugins/commander/commands/uptime.js
@@ -13,33 +13,30 @@ class RuntimeCommand extends Command {
         ];
     }
 
-    call(message) {
-        const client = bot.client;
-        const time = client.uptime;
+    toTimeString(time) {
+        const f = Math.floor,
+            s = f(time / 1000),
+            m = f(s / 60),
+            h = f(m / 60),
+            d = f(h / 24);
 
-        const toTimeString = (time) => {
-            const f = Math.floor,
-                s = f(time / 1000),
-                m = f(s / 60),
-                h = f(m / 60),
-                d = f(h / 24);
+        const p = (s) => s > 1 ? 's' : '';
 
-            const p = (s) => s > 1 ? 's' : '';
+        const seconds = `${s % 60} second${p(s)}`;
+        const minutes = `${m % 60} minute${p(m)}`;
+        const hours = `${h % 24} hour${p(h)}`;
 
-            const seconds = `${s % 60} second${p(s)}`;
-            const minutes = `${m % 60} minute${p(m)}`;
-            const hours = `${h % 24} hour${p(h)}`;
+        if (d) {
+            const days = `${d} day${p(d)}`;
 
-            if (d) {
-                const days = `${d} day${p(d)}`;
-
-                return `${days}, ${hours}, ${minutes} and ${seconds} `;
-            }
-
-            return `${hours}, ${minutes} and ${seconds}`;
+            return `${days}, ${hours}, ${minutes} and ${seconds} `;
         }
 
-        const uptime = toTimeString(time);
+        return `${hours}, ${minutes} and ${seconds}`;
+    }
+
+    call(message) {
+        const uptime = this.toTimeString(bot.client.uptime);
 
         message.channel.send(`Bot has been up and running for ${uptime}!`);
     }
