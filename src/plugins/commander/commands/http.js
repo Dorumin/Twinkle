8d7a001dc69851b4d6ef18bc4964eed74820c599
Sidemen19 <lavyag01@gmail.com>
2020-09-22 07:48:19 +1000

    requested fixes to http cmd + update rss bot url

--- src/plugins/commander/commands/http.js
+++ src/plugins/commander/commands/http.js
@@ -1,39 +1,36 @@
 const Command = require('../structs/Command');
-const FormatterPlugin = require('../../fandomizer');
+const { MessageAttachment } = require('discord.js');
 
 class HTTPCommand extends Command {
-    static get deps() {
-        return [
-            FormatterPlugin
-        ]
-    }
-
     constructor(bot) {
         super(bot);
-        this.aliases = ['http', 'catus', 'status'];
+        this.aliases = ['http', 'catus'];
 
         this.shortdesc = `Sends a catus code image.`;
         this.desc = `Sends a HTTP error's cat error code image from http.cat`;
         this.usages = [
             '!http 404'
         ];
-    }
 
-    async run(message, content) {
-        if (!content || !parseInt(content)) return;
-        const validCodes = [
+        this.validCodes = [
             100, 101, 102, 103, 200, 202, 203, 204, 205, 206, 207, 300,
             301, 302, 303, 304, 305, 306, 307, 400, 401, 402, 403, 404, 405, 406, 407,
             408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 422, 423, 424, 425,
             426, 449, 450, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510
         ];
+    }
 
-        if (validCodes.includes(parseInt(content))) {
-            await message.channel.send(`https://http.cat/${content}.jpg`);
+    async run(message, content) {
+        if (!content || !parseInt(content)) return;
+
+        if (this.validCodes.includes(parseInt(content))) {
+            const attachment = new MessageAttachment(`https://http.cat/${content}.jpg`, `${content}.jpg`)
+
+            await message.channel.send(`<https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/${content}>`, attachment);
             return;
         }
 
-        await message.channel.send(`Status code ${this.bot.fmt.code(content)} not found.`);
+        await message.channel.send(`https://http.cat/404.jpg`);
     }
 }
 
