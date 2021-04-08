d02d8f3353ab559ab8b180fdc27ebe83309f9e09
Luka Simić <git@kocka.tech>
2021-04-08 10:22:23 +0200

    Add Wiki-Bot as the first option in !rc.

--- src/plugins/commander/commands/rc.js
+++ src/plugins/commander/commands/rc.js
@@ -12,7 +12,7 @@ class RCCommand extends Command {
 
     constructor(bot) {
         super(bot);
-        this.aliases = ['rc', 'rssbot', 'rctodiscord'];
+        this.aliases = ['rc', 'rssbot', 'rctodiscord', 'rcscript'];
 
         this.shortdesc = `Information on setting up RC-to-Discord relay.`;
         this.desc = `Tells you how to successfully set up a RC-to-Discord relay system.`;
@@ -27,8 +27,11 @@ class RCCommand extends Command {
             : this.bot.fmt.code('https://<yourwiki>.fandom.com/wiki/Special:RecentChanges?feed=rss');
 
         message.channel.send(`
-The easiest way to set up a Recent Changes-to-Discord system is by inviting and configuring an RSS bot for your server. A popular one is <https://monitorss.xyz/>.
-When the bot prompts you for the RSS feed/url, enter ${link} in. Note that, unlike <#246076868560814080>, the bot will have a small delay, usually around 10 minutes.
+The easiest way to set up a Recent Changes-to-Discord system is by inviting Wiki-Bot (<https://wikibot.fandom.com/wiki/Wiki-Bot_Wiki>) to your server and setting up its recent changes webhook.
+More information is available on the Wiki-Bot Wiki: <https://wikibot.fandom.com/wiki/Recent_changes_webhook>.
+
+Another way is inviting and configuring an RSS bot for your server. A popular one is <https://monitorss.xyz/>.
+When the bot prompts you for the RSS feed/URL, enter ${link} in. Note that, unlike <#246076868560814080>, this bot has a small delay, usually around 10 minutes.
         `);
     }
 }
