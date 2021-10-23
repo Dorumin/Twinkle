081805b3a09172887d52a8bde7c4386e1bed017f
Naughty <dorminsama@gmail.com>
2021-10-23 19:29:52 -0300

    「Lockdown」

---
+++ src/plugins/automod/filters/Lockdown.js
@@ -0,0 +1,115 @@
+const Cache = require('../../../structs/Cache.js');
+const Filter = require('../structs/Filter.js');
+
+// This Filter is placed here due to structuring reasons,
+// but it's not a real "message filter" that the automod plugin deals with
+// It's not interested in messages and instead manually handles guild joins
+// and locks down a server if too many happen in too short of a time
+class LockdownFilter extends Filter {
+    constructor(automod) {
+        super(automod);
+
+        this.config = automod.config.LOCKDOWN || {};
+        this.joins = this.config.JOINS || 10;
+        this.delay = this.config.DELAY || 15 * 60 * 1000;
+        this.roleId = this.config.MUTE_ROLE_ID || '401231955741507604';
+        this.channelId = this.config.CHANNEL || '246075715714416641';
+        this.lockdownMessage = this.config.MESSAGE || '⚠️ Entering server lockdown';
+
+        this.guildMap = new Cache();
+
+        this.lockdownGuilds = new Set();
+
+        automod.bot.client.on('guildMemberAdd', this.onJoin.bind(this));
+    }
+
+    interested(message) {
+        return false;
+    }
+
+    handle() {}
+
+    onJoin(member) {
+        if (!member.guild) return;
+
+        if (this.lockdownGuilds.has(member.guild.id)) {
+            this.customHandle(member);
+            return;
+        }
+
+        let set;
+        if (!this.guildMap.has(member.guild.id) ) {
+            set = new Set();
+            this.guildMap.set(member.guild.id, set);
+        } else {
+            set = this.guildMap.get(member.guild.id);
+        }
+
+        set.add(member.user.id);
+
+        setTimeout(() => {
+            set.delete(member.user.id);
+        }, this.delay);
+
+        if (set.size > this.joins) {
+            this.lockdown(member.guild).catch(this.automod.bot.reportError.bind(this.automod.bot, 'Error in lockdown... oh no'));
+        }
+    }
+
+    async lockdown(guild) {
+        this.lockdownGuilds.add(guild.id);
+
+        const channel = guild.channels.cache.get(this.channelId);
+
+        const promises = [];
+
+        if (channel) {
+            promises.push(channel.send(this.lockdownMessage));
+        }
+
+        for (const id of this.guildMap.get(guild.id)) {
+            const member = member.guild.members.cache.get(id);
+
+            if (member) {
+                promises.push(this.customHandle(member));
+            }
+        }
+
+        await Promise.all(promises);
+    }
+
+    async customHandle(member) {
+        const muteAction = member.roles.add(this.roleId);
+        const muteResult = await muteAction.then(() => 'and muted', () => 'but could not be muted');
+
+        let logMessage = `**Reason**: Lockdown\n<@${member.id}>`;
+
+        // try {
+        //     await message.author.send(`Hello. You were grounded for joining within a period considered a raid. Contact a server moderator if this was wrongful.`);
+        // } catch (error) {
+        //     if (error && error.code === 50007) {
+        //         logMessage += '\nUser blocked DMs.';
+        //     } else {
+        //         await this.automod.bot.reportError('Failed to warn user:', error);
+        //         logMessage += '\nFailed to warn user.';
+        //     }
+        // }
+
+        const reporting = await this.automod.logchan();
+
+        if (!reporting) return;
+
+        await reporting.send({
+            embeds: [{
+                author: {
+                    name: `${message.author.tag} has been picked up for lockdown ${muteResult}`,
+                    icon_url: message.author.displayAvatarURL()
+                },
+                color: message.guild.me.displayColor,
+                description: logMessage,
+            }]
+        });
+    }
+}
+
+module.exports = LockdownFilter;
---
+++ src/plugins/commander/commands/lockdown.js
@@ -0,0 +1,81 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const ModCommand = require('../structs/ModCommand.js');
+const AutoModPlugin = require('../../automod');
+const LockdownFilter = require('../../automod/filters/Lockdown');
+
+class LockdownCommand extends ModCommand {
+    static get deps() {
+        return [
+            AutoModPlugin
+        ];
+    }
+
+    constructor() {
+        super(bot);
+        this.aliases = ['lockdown', 'raid'];
+        this.schema = new SlashCommandBuilder()
+            .addIntegerOption(option =>
+                option.setName('enabled')
+                    .setDescription('Must be on or off. Missing to toggle')
+            );
+
+        this.hidden = true;
+        this.shortdesc = `Enables lockdown mode.`;
+        this.desc = `
+                    Locks down the server so new members will be muted on join.
+                    Also called "raid mode".
+                    Pass "on" or "off" as the first argument to explicitly toggle.
+                    You need to be a moderator to use this command.`;
+        this.usages = [
+            '!lockdown <on/off>'
+        ];
+        this.examples = [
+            '!lockdown',
+            '!raid on',
+            '!raid off'
+        ];
+
+        this.lockdown = this.bot.automod.filters.find(filter => filter instanceof LockdownFilter);
+    }
+
+    async call(message, content) {
+        if (!message.guild) {
+            await message.channel.send('Use this in a guild');
+            return;
+        }
+
+        let enable;
+        switch (content.toLowerCase()) {
+            case 'on':
+                enable = true;
+                break;
+            case 'off':
+                enable = false;
+                break;
+            default:
+                enable = !this.lockdown.lockdownGuilds.has(message.guild.id);
+                break;
+        }
+
+        if (enable === this.lockdown.lockdownGuilds.has(message.guild.id)) {
+            if (enable) {
+                await message.channel.send('The guild is already locked down');
+            } else {
+                await message.channel.send('The guild is not locked down');
+            }
+
+            return;
+        }
+
+
+        if (enable) {
+            this.lockdown.lockdownGuilds.add(message.guild.id);
+            await message.channel.send('The guild has been locked down');
+        } else {
+            this.lockdown.lockdownGuilds.delete(message.guild.id);
+            await message.channel.send('The guild has been freed');
+        }
+    }
+}
+
+module.exports = LockdownCommand;
