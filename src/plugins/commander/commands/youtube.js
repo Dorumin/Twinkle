b162eb187a5f580c2aedec25d71b13475aee5846
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2022-06-29 00:07:10 -0800

    actually copy the right thing

--- src/plugins/commander/commands/youtube.js
+++ src/plugins/commander/commands/youtube.js
@@ -1,41 +1,117 @@
-const got = require('got');
-const Plugin = require('../../structs/Plugin');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Command = require('../structs/Command.js');
+const YouTubePlugin = require('../../youtube');
+const CommandUtilsPlugin = require('../../command-utils/index.js');
+const FormatterPlugin = require('../../fmt/index.js');
 
-class YouTubePlugin extends Plugin {
-    load() {
-        this.bot.youtube = new YouTube(this.bot);
+class YouTubeCommand extends Command {
+    static get deps() {
+        return [
+            YouTubePlugin,
+            CommandUtilsPlugin,
+            FormatterPlugin
+        ];
     }
-}
 
-class YouTube {
     constructor(bot) {
-        this.bot = bot;
-        this.config = bot.config.YOUTUBE || {};
-        this.tokens = this.config.TOKENS || [];
+        super(bot);
+        this.aliases = ['youtube', 'yt', 'yts'];
+        this.schema = new SlashCommandBuilder()
+            .addStringOption(option =>
+                option.setName('query')
+                    .setDescription('The search query')
+                    .setRequired(true)
+            );
 
-        if (this.tokens.length === 0 && this.config.TOKEN) {
-            this.tokens.push(this.config.TOKEN);
+        this.shortdesc = `Searches youtube`;
+        this.desc = `
+            Searches YouTube for a given search query
+            The prefix "yts" provides a set of results
+            `;
+        this.usages = [
+            '!yt Ken Ashcorp',
+            '!yts Tom Scott'
+        ];
+    }
+
+    async call(message, content, { alias }) {
+        if (!content) {
+            await message.channel.send('Please provide a search query.');
+            return;
+        }
+
+        const results = await this.bot.youtube.search(content);
+
+        if (results.length === 0) {
+            await message.channel.send('Sorry, no results!');
+            return;
+        }
+
+        if (alias === 'yts') {
+            await this.postResults(message, results);
+        } else {
+            await message.channel.send(`https://youtu.be/${results[0].id.videoId}`);
         }
     }
 
-    getToken() {
-        const index = Math.floor(Math.random() * this.tokens.length);
+    async postResults(originalMessage, results) {
+        if (results.length > 5) {
+            results = results.slice(0, 5);
+        }
 
-        return this.tokens[index];
+        const content = this.bot.fmt.codeBlock('asc',
+            results.map((result, i) => `[${i + 1}] ${result.snippet.title}`)
+                .join('\n')
+        );
+        const message = await originalMessage.channel.send(content);
+
+        const manager = this.bot.commandUtils.reactionManager(message)
+            .timeout(30000)
+            .onTimedOut(() => {
+                message.edit('Sorry, you took too long!');
+            })
+            .addUserId(originalMessage.author.id);
+
+        for (let index = 0; index < results.length; index++) {
+            manager.addReaction(
+                this.getEmoji(index + 1),
+                this.onResultChoose.bind(this, {
+                    message,
+                    results,
+                    index,
+                    manager
+                })
+            );
+        }
+
+        await manager.listen();
     }
 
-    async search(query) {
-        const results = await got('https://www.googleapis.com/youtube/v3/search', {
-            searchParams: {
-                part: 'snippet',
-                q: query,
-                key: this.getToken(),
-                type: 'video'
-            }
-        }).json();
+    getEmoji(index) {
+        switch (index) {
+            case 1: return '1️⃣';
+            case 2: return '2️⃣';
+            case 3: return '3️⃣';
+            case 4: return '4️⃣';
+            case 5: return '5️⃣';
+            default: return '🤷‍♀️';
+        }
+    }
 
-        return results.items;
+    async onResultChoose({ message, results, index, manager }) {
+        // Discard all reactions, likely to fail because message is attempted
+        // to be deleted next
+        manager.clear().catch(() => {});
+
+        const result = results[index];
+
+        try {
+            await Promise.all([
+                message.delete(),
+                message.channel.send(`https://youtu.be/${result.id.videoId}`)
+            ]);
+        } catch(e) {}
     }
 }
 
-module.exports = YouTubePlugin;
+module.exports = YouTubeCommand;
